9c599c3039447654391054ad12001acd1d36cc22
Mark Heymann <mark.heymann01@gmail.com>
2024-08-02 22:49:12 -0400

    idk

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AboutComponent} from './profile/about/about/about.component';
+import { ProjectsComponent} from './profile/projects/projects/projects.component';
+import { ContactComponent} from './profile/contact/contact/contact.component';
+import { ProfileComponent} from './profile/profile/profile.component';
+export const routes: Routes = [
+  {path: '', component: ProfileComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contact', component: ContactComponent}
+];
