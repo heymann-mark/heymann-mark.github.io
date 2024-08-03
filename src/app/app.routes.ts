b6e4af45a5f6cd6a129f45ebd6d110e8c7f9ff59
Mark Heymann <mark.heymann01@gmail.com>
2024-08-03 17:15:35 -0400

    ugh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { AboutComponent} from './profile/about/about/about.component';
-import { ProjectsComponent} from './profile/projects/projects/projects.component';
-import { ContactComponent} from './profile/contact/contact/contact.component';
-import { ProfileComponent} from './profile/profile/profile.component';
+import { AboutComponent} from './profile/about/about.component';
+import { ProjectsComponent} from './profile/projects/projects.component';
+import { ContactComponent} from './profile/contact/contact.component';
+import { ProfileComponent} from './profile/profile.component';
 export const routes: Routes = [
   {path: '', component: ProfileComponent},
   {path: 'about', component: AboutComponent},
