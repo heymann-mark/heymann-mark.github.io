54b485f3c80d33e12822de36b588af1d1483e631
Mark Heymann <mark.heymann01@gmail.com>
2024-08-02 22:24:16 -0400

    made the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,11 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  //templateUrl: './app.component.html',
-  template:
-  `Hello  {{ city }}, {{ 1 + 1 }}`,
-  //styleUrl: './app.component.css'
-  styles: `
-    :host {
-      color: blue;
-    }
-  `,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'heymann-mark.github.io';
-  city = 'San Francisco';
+
 }
