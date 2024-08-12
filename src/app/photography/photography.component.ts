3cea8620c5be8178e39bfb83a2df802490a741de
Mark Heymann <mark.heymann01@gmail.com>
2024-08-11 22:45:43 -0400

    photography

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from '../profile/header/header.component';
 @Component({
   selector: 'app-photography',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule,HeaderComponent],
   templateUrl: './photography.component.html',
   styleUrl: './photography.component.css'
 })
 export class PhotographyComponent {
   constructor(private route : ActivatedRoute){}
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
